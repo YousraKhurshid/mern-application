a399e6a17f25a50f1e91b93fa01f3c9be9d982fe
YousraKhurshid <yousrakhurshid1@gmail.com>
2023-09-01 20:51:47 -0700

    updated

--- client/src/Guest/Components/GuestCards.jsx
+++ client/src/Guest/Components/GuestCards.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import GuestCards from '../Components/GuestCatds'
+import axios from 'axios'
+import './card.css'
 
-export default function GuestCards({ name, image }) {
+export default function Brands() {
+    const [brands, setBrands] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/get-all-brands').then(json => setBrands(json.data.brands)).catch(err => alert(err.message))
+    }, [])
+
+
     return (
-        <div className="col-md-3">
-            <Link to={`/${name.split(' ').join('-')}`} className='text-decoration-none'>
-                <div className="card">
-                    <img src={image} className="card-img-top p-5" alt={name} style={{ height: '20vh', objectFit: 'contain' }} />
-                    <div className="card-body">
-                        <h5 className="card-title">{name}</h5>
-                    </div>
-                </div>
-            </Link>
+     <div className="container my-5">
+        <div className="text-center">
+            <h2>Brands</h2>
+            <small className="text-secondary">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum delectus magnam doloribus voluptatibus possimus corrupti aliquid itaque harum debitis ipsa!</small>
         </div>
+
+        <div className="row my-5">
+            {
+                brands?.map((val, key) => <GuestCards key={key} image={val.BrandImage} name={val.BrandName} url={`/brands/${val.BrandName}`}/>)
+            }
+
+        </div>
+    </div>
     )
 }
--- client/src/Guest/Pages/Home.jsx
+++ client/src/Guest/Pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Brand from '../Components/Brands'
+import Brands from '../Components/Brands'
 import Category from '../Components/Category'
 
 export default function Home() {
     return (
         <div>
-            <Category />
-            <Brand />
+            <Category/>
+            <Brands/>
         </div>
     )
 }
