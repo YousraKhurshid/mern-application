ab351cb0e6ce89b8a851e6832e7790a4a4819d4c
YousraKhurshid <yousrakhurshid1@gmail.com>
2023-09-02 01:21:05 -0700

    f

--- client/src/User/Pages/Category.jsx
+++ client/src/User/Pages/Category.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import UserCards from '../Components/UserCards'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './card.css'; 
 
 export default function Category() {
-    const data = [
-        {
-            "_id": "64b9220ffd82e7fc615b134a",
-            "BrandName": "Makeup City",
-            "BrandImage": "https://www.makeupcityshop.com/cdn/shop/files/MUC-Logo-new-01-footer.png?v=1624558798",
-            "__v": 0
-        },
-        {
-            "_id": "64c0f917e95a2a452eace38e",
-            "BrandName": "Sana Safinaz",
-            "BrandImage": "https://www.sanasafinaz.com/static/version1688385473/frontend/RLTSquare/SanaSafinaz/en_US/images/S-SBlack.png",
-            "__v": 0
-        },
-    ]
+    const [category, setcategory] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/get-all-categories')
+            .then(json => {
+                if (json.data.category) {
+                    setcategory(json.data.category);
+                }
+            })
+            .catch(err => alert(err.message));
+    }, []);
+    
 
     return (
         <div className="container my-5">
@@ -23,13 +21,18 @@ export default function Category() {
                 <h2>Category</h2>
                 <small className="text-secondary">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum delectus magnam doloribus voluptatibus possimus corrupti aliquid itaque harum debitis ipsa!</small>
             </div>
-
+    
             <div className="row my-5">
-                {
-                    data.map((val, key) => <UserCards key={key} image={val.BrandImage} name={val.BrandName} />)
-                }
-
+                {category?.map((val, key) => (
+                    <div key={key} className="col-md-4">
+                        <div className="card">
+                            <img className="card-image" src={val.CategoryImage} alt={val.CategoryName} />
+                            <h3 className="card-name">{val.CategoryName}</h3>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
-    )
+    );
+    
 }
